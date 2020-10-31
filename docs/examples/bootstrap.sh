2d5f943e7496a40ce28584a6ecd43e5292129932
Max Rees <maxcrees@me.com>
2020-10-31 00:03:36 -0400

    docs/examples/bootstrap.sh: forcefully install /etc/localtime

--- docs/examples/bootstrap.sh
+++ docs/examples/bootstrap.sh
@@ -7,12 +7,12 @@
 # If the hosts(5) and/or resolv.conf(5) are unmodified, apk will try to
 # overwrite them even though they are read-only. So we work around it
 # using symlinks.
-rm -f /etc/hosts /etc/resolv.conf
-ln -sr /af/hosts /etc/hosts
-ln -sr /af/resolv.conf /etc/resolv.conf
+ln -srf /af/hosts /etc/hosts
+ln -srf /af/resolv.conf /etc/resolv.conf
+
+ln -srf /usr/share/zoneinfo/UTC /etc/localtime
 
 apk upgrade -Ual
-ln -sr /usr/share/zoneinfo/UTC /etc/localtime
 
 af_mkuser
 af_userconf
