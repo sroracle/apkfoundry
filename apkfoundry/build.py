sroracle/apkfoundry

commit f1ed1da895b9e20b6b84535058de25b4ef2253fc

Rename -s, --srcdest to --cache-src

apkfoundry/build.py
@@ -435,8 +435,8 @@ def _buildrepo_args(args):
         configuration, otherwise none)""",
     )
     cont.add_argument(
-        "-s", "--srcdest",
-        help="external source file directory (default: none)",
+        "--cache-src",
+        help="external source file cache directory (default: none)",
     )
 
     checkout = opts.add_argument_group(
@@ -508,10 +508,10 @@ def _buildrepo_bootstrap(opts, cdir):
         opts.repodest = Path(opts.repodest)
         if not _ensure_dir(opts.repodest):
             return None
-    if opts.srcdest:
-        cont_make_args += ["--srcdest", opts.srcdest]
-        opts.srcdest = Path(opts.srcdest)
-        if not _ensure_dir(opts.srcdest):
+    if opts.cache_src:
+        cont_make_args += ["--cache-src", opts.cache_src]
+        opts.cache_src = Path(opts.cache_src)
+        if not _ensure_dir(opts.cache_src):
             return None
     if opts.cache:
         cont_make_args += ["--cache", opts.cache]

apkfoundry/container.py
@@ -453,7 +453,7 @@ def _make_infodir(conf, opts):
         "aportsdir": opts.aportsdir, # this make act weird since
                                      # it's always specified
         "repodest": opts.repodest,
-        "srcdest": opts.srcdest,
+        "srcdest": opts.cache_src,
     }
 
     for mount in mounts:
@@ -510,9 +510,9 @@ def _cont_make_args(args):
         help="""setarch(8) architecture name (default: look in site
         configuration, otherwise none)""",
     )
-    opts.add_argument(
-        "-s", "--srcdest",
-        help="external source file directory (default: none)",
+    cont.add_argument(
+        "--cache-src",
+        help="external source file cache directory (default: none)",
     )
     opts.add_argument(
         "cdir", metavar="CDIR",

libexec/gl-run
@@ -148,7 +148,7 @@ def build_script(script, env, privkey, pubkey):
         "--branch", env.ref,
         "--cache", env.cache,
         "--delete", "never",
-        "--srcdest", env.srcdest,
+        "--cache-src", env.srcdest,
         "--script", "/af/scripts/gl-build",
     ]
 

commit 4139b3ac27a9a30cf9209a91209c267f8f16daf2

Add compat shims for removed CLI options
This smooths the breakage from:

f1ed1da ("Rename -s, --srcdest to --cache-src")
516728e ("Rename -c, --cache to --cache-apk")
b262318 ("Remove -s short options for uncommon parameters")

apkfoundry/build.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2019-2020 Max Rees
 # See LICENSE for more information.
-import argparse   # ArgumentParser
+import argparse   # ArgumentParser, SUPPRESS
 import enum       # Enum, IntFlag, unique
 import functools  # partial
 import logging    # getLogger
@@ -421,14 +421,17 @@ def _buildrepo_args(args):
         help=f"""APK architecture name (default:
         {apkfoundry.DEFAULT_ARCH})""",
     )
+    cont.add_argument("-A", help=argparse.SUPPRESS)
     cont.add_argument(
         "--cache-apk",
         help="external APK cache directory (default: none)",
     )
+    cont.add_argument("-c", "--cache", help=argparse.SUPPRESS)
     cont.add_argument(
         "--cache-src",
         help="external source file cache directory (default: none)",
     )
+    cont.add_argument("-s", "--srcdest", help=argparse.SUPPRESS)
     cont.add_argument(
         "--directory", metavar="CDIR",
         help=f"""use CDIR as the container root (default: temporary
@@ -439,6 +442,7 @@ def _buildrepo_args(args):
         help="""setarch(8) architecture name (default: look in site
         configuration, otherwise none)""",
     )
+    cont.add_argument("-S", help=argparse.SUPPRESS)
 
     checkout = opts.add_argument_group(
         title="Checkout options",
@@ -503,7 +507,21 @@ def _buildrepo_args(args):
         "startdirs", metavar="STARTDIR", nargs="*",
         help="list of STARTDIRs to build",
     )
-    return opts.parse_args(args)
+    opts = opts.parse_args(args)
+    if opts.A:
+        _LOGGER.warning("-A is deprecated. Use --arch.")
+        opts.arch = opts.A
+    if opts.cache:
+        _LOGGER.warning("-c/--cache is deprecated. Use --cache-apk.")
+        opts.cache_apk = opts.cache
+    if opts.srcdest:
+        _LOGGER.warning("-s/--srcdest is deprecated. Use --cache-src.")
+        opts.cache_src = opts.srcdest
+    if opts.S:
+        _LOGGER.warning("-S is deprecated. Use --setarch.")
+        opts.setarch = opts.S
+
+    return opts
 
 def _buildrepo_bootstrap(opts, cdir):
     _log.section_start(

apkfoundry/container.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2019-2020 Max Rees
 # See LICENSE for more information.
-import argparse   # ArgumentParser
+import argparse   # ArgumentParser, SUPPRESS
 import json       # load
 import logging    # getLogger
 import os         # close, environ, fdopen, getgid, getuid, listdir, pipe, write
@@ -487,6 +487,7 @@ def _cont_make_args(args):
         help=f"""APK architecture name (default:
         {apkfoundry.DEFAULT_ARCH})""",
     )
+    opts.add_argument("-A", help=argparse.SUPPRESS)
     opts.add_argument(
         "--branch",
         help="""git branch for APORTSDIR (default: detect). This is
@@ -496,10 +497,12 @@ def _cont_make_args(args):
         "--cache-apk",
         help="external APK cache directory (default: none)",
     )
+    opts.add_argument("-c", "--cache", help=argparse.SUPPRESS)
     opts.add_argument(
         "--cache-src",
         help="external source file cache directory (default: none)",
     )
+    opts.add_argument("-s", "--srcdest", help=argparse.SUPPRESS)
     opts.add_argument(
         "--no-pubkey-copy", action="store_true",
         help="do not copy public keys to REPODEST",
@@ -509,11 +512,13 @@ def _cont_make_args(args):
         help="""external package destination directory (default:
         none)""",
     )
+    opts.add_argument("-r", help=argparse.SUPPRESS)
     opts.add_argument(
         "--setarch",
         help="""setarch(8) architecture name (default: look in site
         configuration, otherwise none)""",
     )
+    opts.add_argument("-S", help=argparse.SUPPRESS)
     opts.add_argument(
         "cdir", metavar="CDIR",
         help="container directory",
@@ -522,7 +527,24 @@ def _cont_make_args(args):
         "aportsdir", metavar="APORTSDIR",
         help="project git directory",
     )
-    return opts.parse_args(args)
+    opts = opts.parse_args(args)
+    if opts.A:
+        _LOGGER.warning("-A is deprecated. Use --arch.")
+        opts.arch = opts.A
+    if opts.cache:
+        _LOGGER.warning("-c/--cache is deprecated. Use --cache-apk.")
+        opts.cache_apk = opts.cache
+    if opts.srcdest:
+        _LOGGER.warning("-s/--srcdest is deprecated. Use --cache-src.")
+        opts.cache_src = opts.srcdest
+    if opts.r:
+        _LOGGER.warning("-r is deprecated. Use --repodest.")
+        opts.repodest = opts.r
+    if opts.S:
+        _LOGGER.warning("-S is deprecated. Use --setarch.")
+        opts.setarch = opts.S
+
+    return opts
 
 def cont_make(args):
     opts = _cont_make_args(args)

bin/af-chroot
@@ -2,7 +2,8 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2019-2020 Max Rees
 # See LICENSE for more information.
-import argparse # ArgumentParser, REMAINDER
+import argparse # ArgumentParser, REMAINDER, SUPPRESS
+import logging  # warning
 import sys      # exit
 from pathlib import Path
 
@@ -31,6 +32,7 @@ opts.add_argument(
     "--repo",
     help="change repository to REPO",
 )
+opts.add_argument("-r", help=argparse.SUPPRESS)
 opts.add_argument(
     "--ro-aports", action="store_true",
     help="mount APORTSDIR as read-only",
@@ -55,6 +57,10 @@ opts = opts.parse_args()
 opts.cdir = Path(opts.cdir).resolve(strict=False)
 if not opts.argv:
     opts.argv = ["sh", "-"]
+
+if opts.r:
+    logging.warning("-r is deprecated. Use --repo.")
+    opts.repo = opts.r
 
 cont = apkfoundry.container.Container(
     opts.cdir,